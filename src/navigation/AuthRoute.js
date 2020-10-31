f6a4fd47f686febfa498d02496d30f6b57b1fe5d
Alina Turbina <alina.turbina@tum.de>
2020-10-31 17:25:57 +0100

    Add auth error handling
    Add auth error handling

--- src/navigation/AuthRoute.js
+++ src/navigation/AuthRoute.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import auth from '@react-native-firebase/auth';
+import {Alert} from 'react-native';
 
 /**
  * This provider is created
@@ -21,6 +22,14 @@ export const AuthRoute = ({ children }) => {
                         await auth().signInWithEmailAndPassword(email, password);
                     } catch (e) {
                         console.log(e);
+                        switch(e.code) {
+                            case 'auth/wrong-password':
+                                Alert.alert('The password is invalid.');
+                                break;
+                            case 'auth/invalid-email':
+                                Alert.alert('The email address is badly formatted.');
+                                break;
+                        }
                     }
                 },
                 register: async (email, password) => {
@@ -28,6 +37,17 @@ export const AuthRoute = ({ children }) => {
                         await auth().createUserWithEmailAndPassword(email, password);
                     } catch (e) {
                         console.log(e);
+                        switch(e.code) {
+                            case 'auth/email-already-in-use':
+                                Alert.alert('The email address is already in use by another account.');
+                                break;
+                            case 'auth/weak-password':
+                                Alert.alert('The given password is invalid.');
+                                break;
+                            case 'auth/invalid-email':
+                                Alert.alert('An email address must be provided or is badly formatted.');
+                                break;
+                        }
                     }
                 },
                 logout: async () => {
